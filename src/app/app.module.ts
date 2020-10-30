053f74de56e0b8f9c315ab3825134a8cf5f4b25e
PalomaPB <palomabarroqueiro@outlook.com>
2020-10-30 18:16:15 -0300

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,20 @@ import { BitcoinComponent } from '../bitcoin/bitcoin.component';
 import { MinhacarteiraComponent } from '../minhacarteira/minhacarteira.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BitcoinService } from '../bitcoin.service';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
+    CommonModule,
+    BrowserModule,
     FormsModule,
     HttpClientModule, 
     RouterModule.forRoot([
       {path:'bitcoin', component: BitcoinComponent},
       {path:'carteira', component: MinhacarteiraComponent}
     ]) ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, BitcoinComponent, MinhacarteiraComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ BitcoinService ]
 })
--- src/minhacarteira/minhacarteira.component.ts
+++ src/minhacarteira/minhacarteira.component.ts
@@ -1,4 +1,6 @@
+import { formatNumber } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { BitcoinService } from '../bitcoin.service';
 
 @Component({
   selector: 'app-minhacarteira',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./minhacarteira.component.css']
 })
 export class MinhacarteiraComponent implements OnInit {
+  valor_carteira = 1000;
 
-  constructor() { }
+  constructor(public bitcoinService: BitcoinService) { }
 
   ngOnInit() {
+    this.update();
+  }
+
+  update(){
+    this.bitcoinService.update();
+  }
+
+  getPrice(){
+    return this.bitcoinService.currentPrice;
+  }
+
+  comprarBitcoin(){
+    this.valor_carteira += 50;
+    this.update();
+  }
+
+  venderBitcoin(){
+    this.valor_carteira -= 50;
   }
 
 }
